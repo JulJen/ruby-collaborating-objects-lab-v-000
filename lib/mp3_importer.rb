b88b7005a3932b98a26a9f06052c4b66fd0524ca
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-18 17:04:39 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,10 +8,23 @@ class MP3Importer
 
   def files
     files = Dir.entries(@path)
+<<<<<<< HEAD
     files.delete_if { |x| x == "." || x ==".." }
   end
 
   def import
     files.each { |f| Song.new_by_filename(f) }
   end
+=======
+    files.delete_if { |item| item == "." || item ==".."}
+  end
+
+  def import(files)
+  files.each { |filename| Song.new_by_filename(filename) }
+  end
+
+  # def import(list_of_filenames)
+  #     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  # end
+>>>>>>> 33b847409c8cab138814c782c4a28f4c539e05a7
 end
